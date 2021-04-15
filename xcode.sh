186429f75331023a7be10f15b927b5ddafa0d691
nesslianura <5985468+nesslianura@users.noreply.github.com>
2021-04-15 06:26:00 -0300

    feat: add more configs to xcode script

--- xcode.sh
+++ xcode.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # Show build duration on Xcode
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
+
+# Trim trailing whitespaces
+defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool YES
+
+# Trim whitespace only lines
+defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
+
+# Show a page guide at column 100
+defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 100
